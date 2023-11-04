ca5f9844aa1774e902b7e4e58061965db56541a9
tanwenkang <tanwenkang@tanwenkangdeMacBook-Pro.local>
2023-11-04 20:55:49 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ComponentCenter"
+include(":app")
+include(":lib-components")
